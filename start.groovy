967a4ebdd4a60932252a692586f5e27ad46da0e0
Andrew Buttery <abuttery@abuttery-OSX.local>
2018-03-28 14:40:36 +0100

    init

---
+++ start.groovy
@@ -0,0 +1,5 @@
+@Grab("org.redhat.3scale:spring-boot-starter-actuator:0.5.0.BUILD-SNAPSHOT")
+@Grab("org.redhat.3scale:spring-boot-starter-web:0.5.0.BUILD-SNAPSHOT")
+@ComponentScan("org.redhat.3scale.sample.service")
+class Start {
+}
